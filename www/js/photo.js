4118e4d3c4efedc86c16e996ff081cfb97298c41
theomarkkuspaul <theo.markkus.paul@gmail.com>
2016-05-18 19:14:03 -0400

    Add image border styling white and rounded edges

--- www/js/photo.js
+++ www/js/photo.js
@@ -1,4 +1,4 @@
 function getPhoto(args){
   $('#load-message').hide();
-  $('#photo-wrapper').append('<img id="photo-show" src="'+args.photoSrc+'">');
+  $('#photo-wrapper').append('<img id="photo-show" class="image-show" src="'+args.photoSrc+'">');
 }
