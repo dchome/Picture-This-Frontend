95442cf41d9ceee5bb96292812b533c9de528984
theomarkkuspaul <theo.markkus.paul@gmail.com>
2016-05-23 17:14:00 -0400

    Hotfix contacts not displaying, apply conditional statement to prevent contacts with long names

--- platforms/browser/www/js/contacts.js
+++ platforms/browser/www/js/contacts.js
@@ -7,9 +7,12 @@ function getContacts(args) {
 }
 
 function onContactsSuccess(contacts){
-    for (var i = 1; i < contacts.length; i++) {
+
+    for (var i = 0; i < contacts.length; i++) {
       $('#load-message').hide();
-      $('#all-contacts-list').append('<div class="contact-listing"><label class="label-for-contact" for="contacts-'+i+'">'+contacts[i].name.formatted+'</label><input type="checkbox" class="contact-checkbox" id="contacts-'+i+'" value="'+contacts[i].phoneNumbers[0].value+'"></div><br>');
+      if(contacts[i].phoneNumbers != null && contacts[i].name.formatted.length < 18){
+        $('#all-contacts-list').append('<div class="contact-listing"><label class="label-for-contact" for="contacts-'+(i+1)+'">'+contacts[i].name.formatted+'</label><input type="checkbox" class="contact-checkbox" id="contacts-'+(i+1)+'" value="'+contacts[i].phoneNumbers[0].value+'"></div><br>');
+      }
     }
     $('#all-contacts-list').append('<div id="contact-button-wrapper"><input type="submit" id="button-to-contacts-confirm" value="Select"></div>')
 }
